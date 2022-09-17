41e5d87344060607d3c4c2f5b179361a0abd2dee
Omar <muna9068@colorado.edu>
2022-09-17 16:40:18 -0600

    Initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+# Standard Libraries
+import math
+import sys
+from os import rename
+
+# Third party packages
+import requests
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
